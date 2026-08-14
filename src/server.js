95da19747eb392adae41b6d220ba71218837817a
MayankkShrivastavaa <mshri252@gmail.com>
2026-08-14 09:59:23 +0530

    initial Setup

---
+++ src/server.js
@@ -0,0 +1,25 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8"]);
+
+const express = require("express");
+
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+// MiddleWare
+app.use(express.json());
+
+connectDB();
+
+app.get("/", (req, res) => {
+  res.status("Hello From Server");
+});
+
+let Port = process.env.PORT;
+
+app.listen(Port, (err) =>
+  err ? console.log(err) : console.log(`Server is Running at Port ${Port}`),
+);
