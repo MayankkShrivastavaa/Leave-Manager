7cd196a11a8fa92fb057936d63afbe99dd252509
MayankkShrivastavaa <mshri252@gmail.com>
2026-08-21 23:13:01 +0530

    feat: added logic & route for fetching and updating profile details via auth

--- src/controllers/employeeController.js
+++ src/controllers/employeeController.js
@@ -112,11 +112,109 @@ const loginEmployee = async (req, res) => {
     );
 
     return res.status(200).json({ msg: "Login Successfull", token });
-    
   } catch (error) {
     console.log(error);
     return res.status(500).json({ msg: "Internal Server Error" });
   }
 };
 
-module.exports = { signupEmployee, loginEmployee };
+// Get My Profile
+const getProfile = async (req, res) => {
+  try {
+    let employeeId = req.employeeId;
+    const employee = await employeeModel
+      .findById(employeeId)
+      .select("-password");
+
+    if (!employee) {
+      return res.status(404).json({ msg: "Employee Not Found" });
+    }
+
+    return res
+      .status(200)
+      .json({ msg: "Profile Fetched Successfully", employee });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ msg: "Internal Server Error" });
+  }
+};
+
+// Update Profile
+const updateProfile = async (req, res) => {
+  
+  try {
+    let employeeId = req.employeeId;
+    let employeeData = req.body;
+
+    if (!employeeData || Object.keys(employeeData).length === 0) {
+      return res.status(400).json({ msg: "Bad Request! Enter Data to Update" });
+    }
+
+    let { fullName, email, department, password } = employeeData;
+
+    if (fullName !== undefined) {
+      if (!isValid(fullName)) {
+        return res.status(400).json({ msg: "FullName is Required" });
+      }
+
+      if (fullName.trim().length < 2 || !isValidName(fullName)) {
+        return res.status(400).json({ msg: "Invalid FullName" });
+      }
+    }
+
+    if (email !== undefined) {
+      if (!isValid(email)) {
+        return res.status(400).json({ msg: "Email is Required" });
+      }
+
+      if (!isValidEmail(email)) {
+        return res.status(400).json({ msg: "Invalid Email" });
+      }
+
+      let duplicateEmail = await employeeModel.findOne({
+        email,
+        _id: { $ne: employeeId },
+      });
+
+      if (duplicateEmail) {
+        return res.status(400).json({ msg: "Email Already Exists" });
+      }
+    }
+
+    if (department !== undefined) {
+      if (!isValid(department)) {
+        return res.status(400).json({ msg: "Department is Required" });
+      }
+    }
+
+    if (password !== undefined) {
+      if (!isValid(password)) {
+        return res.status(400).json({ msg: "Password is Required" });
+      }
+
+      if (!isValidPassword(password)) {
+        return res.status(400).json({ msg: "Invalid Password" });
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      employeeData.password = hashedPassword;
+    }
+
+    const update = await employeeModel
+      .findByIdAndUpdate(employeeId, employeeData, { new: true })
+      .select("-password");
+
+    if (!update) {
+      return res.status(404).json({ msg: "Employee Not Found" });
+    }
+
+    return res
+      .status(200)
+      .json({ msg: "Profile Updated Successfully", update });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ msg: "Internal Server Error" });
+  }
+};
+
+module.exports = { signupEmployee, loginEmployee, getProfile, updateProfile };
--- src/routes/employeeRoute.js
+++ src/routes/employeeRoute.js
@@ -4,10 +4,18 @@ const router = express.Router();
 const {
   signupEmployee,
   loginEmployee,
+  getProfile,
+  updateProfile
 } = require("../controllers/employeeController");
+
+const auth = require("../middlewares/auth");
 
 router.post("/signup", signupEmployee);
 
 router.post("/login", loginEmployee);
+
+router.get("/myprofile", auth, getProfile);
+
+router.put("/updateprofile", auth, updateProfile)
 
 module.exports = router;
